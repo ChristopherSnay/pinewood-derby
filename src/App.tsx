e48011393382d740e98a574a38ba9ebe4ad5ad6c
ChristopherSnay <christopher.snay@gmail.com>
2026-03-15 19:39:44 -0400

    updates header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,23 @@ function App() {
     <>
       <AppBar position="static">
         <Toolbar>
-          Pinewood Derby
+          Pinewood Derby 2026
         </Toolbar>
-   
-        </AppBar>
-        <main className="container d-flex flex-wrap gap-5 mt-4">
-          <Overall/>
-          <Heats/>
-          <HeatDetails/>
+      </AppBar>
+      
+      <main className="container d-flex flex-wrap gap-5 mt-4">
+        <Overall />
+        <Heats />
+        <HeatDetails />
+      </main>
 
-        </main>
+      <footer>
+        <div className="container text-center py-3">
+          <small>
+            Made by <a href="https://christophersnay.com" target="_blank" rel="noopener noreferrer">Christopher Snay</a>. Source code on <a href="https://github.com/christophersnay/pinewood-derby" target="_blank" rel="noopener noreferrer">GitHub</a>.
+          </small>
+        </div>
+      </footer>
     </>
   )
 }
